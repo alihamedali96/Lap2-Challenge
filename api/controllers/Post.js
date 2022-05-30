610dd3cd553d70be2750c553b3f3e2f4a78c7a6d
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-30 17:43:49 +0100

    findbyId func

--- api/controllers/Post.js
+++ api/controllers/Post.js
@@ -11,7 +11,7 @@ async function index(req, res) {
 
 async function show(req, res) {
   try {
-    const post = await Posts.findById(req.params.id);
+    const post = await Posts.findById(parseInt(req.params.id));
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ err });

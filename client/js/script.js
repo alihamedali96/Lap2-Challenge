88328fe96aedddf1da1daadd6ae12bb7cedb6cac
Sam Hardiment <samuelghardiment@gmail.com>
2022-05-31 10:27:28 +0100

    display post function

--- client/js/script.js
+++ client/js/script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("form");
-
+const post = document.getElementById("post");
 form.addEventListener("submit", createPost);
 window.addEventListener("hashchange", updateLink);
 
@@ -20,12 +20,33 @@ async function createPost(e) {
     const whatisthis = await response.json();
     // Set up  url - The WillTom way
     window.location.hash = `${whatisthis.id}`;
+    const formContainer = document.getElementById("formContainer");
+    formContainer.innerHTML = "";
+    displayPost(whatisthis);
   } catch (err) {
     console.log(err);
   }
 }
 
 function updateLink() {
-  let hash = window.location.href.substring(1);
+  let hash = window.location.href;
   console.log(hash);
+}
+
+function displayPost(data) {
+  console.log(data);
+  const h2 = document.createElement("h2");
+  const h4 = document.createElement("h4");
+  const p = document.createElement("p");
+  h2.id = `title-${data.id}`;
+  h2.class = "title";
+  h2.textContent = `${data.title}`;
+  h4.id = `author-${data.id}`;
+  h4.class = "author";
+  h4.textContent = `${data.posted_by}`;
+  p.id = `story-${data.id}`;
+  p.class = "story";
+  p.textContent = `${data.story}`;
+
+  post.append(h2, h4, p);
 }
